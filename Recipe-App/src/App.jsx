387aeb6fedbec6b743d5754cbc6176ecc59b743b
UmarAhmad12 <umara6274@gmail.com>
2023-08-25 01:50:20 +0530

    Started working on Featured part

--- Recipe-App/src/App.jsx
+++ Recipe-App/src/App.jsx
@@ -1,3 +1,4 @@
+import Featured from "./components/Featured"
 import TopNav from "./components/TopNav"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <TopNav />
+    <Featured />
     </>
   )
 }
