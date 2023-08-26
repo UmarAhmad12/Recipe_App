092d0037d9bf125ebd40ebac97480ddd9facc7fb
UmarAhmad12 <umara6274@gmail.com>
2023-08-26 22:09:27 +0530

    Started working on TopPick part

--- Recipe-App/src/App.jsx
+++ Recipe-App/src/App.jsx
@@ -1,6 +1,7 @@
 import Delivery from "./components/Delivery"
 import Featured from "./components/Featured"
 import TopNav from "./components/TopNav"
+import TopPicks from "./components/TopPicks"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <TopNav />
     <Featured />
     <Delivery />
+    <TopPicks />
     </>
   )
 }
