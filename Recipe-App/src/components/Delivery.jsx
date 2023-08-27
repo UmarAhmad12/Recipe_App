416bd06707faf99287f81a665c54432f0a7474ca
UmarAhmad12 <umara6274@gmail.com>
2023-08-27 22:46:06 +0530

    Formatted All documents

--- Recipe-App/src/components/Delivery.jsx
+++ Recipe-App/src/components/Delivery.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const Delivery = () => {
   return (
-    <div className='w-full bg-white py-16 px-4'>
-      <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
-      <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp
-' />
-<div className='flex flex-col justify-center'>
-  <p className='text-[#00df9a] font-bold'>Get The App</p>
-  <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae quasi repellat natus accusamus delectus illo atque qui vitae obcaecati. Quasi itaque, corrupti dolorem nisi voluptatem maiores! Voluptate ducimus provident culpa. Rerum aut atque fugiat autem, ipsum accusantium illum ipsam! Quam expedita illum sint, praesentium repellendus blanditiis et deserunt reiciendis iusto nisi voluptas quod? Aspernatur repellat rem nihil quod consectetur et incidunt voluptas fuga, quas suscipit, aliquid ad. Omnis quasi maiores ad saepe quidem sequi maxime id animi quae! Necessitatibus dolores ullam, accusamus obcaecati esse perspiciatis hic nulla placeat. Nostrum numquam repellat temporibus blanditiis doloribus, esse magni exercitationem rerum praesentium.
-  </p>
-  <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
-</div>
+    <div className="w-full bg-white py-16 px-4">
+      <h3 className="text-orange-500 font-bold text-2xl text-center">
+        Quick Delivery App
+      </h3>
+      <div className="w-[1240px] mx-auto grid md:grid-cols-2">
+        <img
+          className="w-[550px] mx-auto my-4"
+          src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp
+"
+        />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">Get The App</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Limitless Convenience on-demand
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            quae quasi repellat natus accusamus delectus illo atque qui vitae
+            obcaecati. Quasi itaque, corrupti dolorem nisi voluptatem maiores!
+            Voluptate ducimus provident culpa. Rerum aut atque fugiat autem,
+            ipsum accusantium illum ipsam! Quam expedita illum sint, praesentium
+            repellendus blanditiis et deserunt reiciendis iusto nisi voluptas
+            quod? Aspernatur repellat rem nihil quod consectetur et incidunt
+            voluptas fuga, quas suscipit, aliquid ad. Omnis quasi maiores ad
+            saepe quidem sequi maxime id animi quae! Necessitatibus dolores
+            ullam, accusamus obcaecati esse perspiciatis hic nulla placeat.
+            Nostrum numquam repellat temporibus blanditiis doloribus, esse magni
+            exercitationem rerum praesentium.
+          </p>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Delivery
+export default Delivery;
