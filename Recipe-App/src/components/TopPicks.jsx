931222a3652842c2b4a9be5d6e11bad977a38f98
UmarAhmad12 <umara6274@gmail.com>
2023-08-27 19:53:49 +0530

    Added slide property in Toppick section

--- Recipe-App/src/components/TopPicks.jsx
+++ Recipe-App/src/components/TopPicks.jsx
@@ -8,9 +8,11 @@ const TopPicks = () => {
     <>
     <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1>
     <div className='hidden lg:flex max-w[1520px] m-auto py-2 px-2'>
+      <Splide options={{perPage: 4,}}>
       {
         topPicks.map((item) => {
           return (
+            <SplideSlide>
             <div className='rounded-3xl relative'>
               <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
                 <p className='px-2'>{item.title}</p>
@@ -19,9 +21,11 @@ const TopPicks = () => {
               <img className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'
                src={item.img}/>
             </div>
+            </SplideSlide>
           )
         })
       }
+      </Splide>
     </div>
     </>
   )
