81fd756212d33e914b5bdd1b85d8e98cf5e78ab7
UmarAhmad12 <umara6274@gmail.com>
2023-08-31 22:08:24 +0530

    Added some meal images

--- Recipe-App/src/components/Meal.jsx
+++ Recipe-App/src/components/Meal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { mealData } from '../data/Data';
 
 const Meal = () => {
   return (
@@ -6,6 +7,15 @@ const Meal = () => {
         <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
         Our Meal
       </h1>
+      <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4'>
+        {
+          mealData.map((item) => (
+            <div className='border-none hover:scale-105 duration-300'>
+              <img src={item.image} />
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
