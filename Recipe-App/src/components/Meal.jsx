0f998c85a2a467de79bc9a153431d5bac8ed95f7
UmarAhmad12 <umara6274@gmail.com>
2023-08-31 22:29:18 +0530

    Meal section under process

--- Recipe-App/src/components/Meal.jsx
+++ Recipe-App/src/components/Meal.jsx
@@ -4,16 +4,20 @@ import { mealData } from '../data/Data';
 const Meal = () => {
   return (
     <div className='max-w[1520px] m-auto px-4 py-12'>
-        <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+        <h1 className="text-orange-500 font-bold text-2xl text-center py-4">
         Our Meal
       </h1>
       <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6'>
         {
           mealData.map((item) => (
             <div className='border-none hover:scale-105 duration-300'>
-              <img src={item.image} 
-              className='w-full h-full object-cover rounded-lg cursor-pointer'
+              <img src={item.image}
+              alt={item.image} 
+              className='w-full h-[200px] object-cover rounded-lg cursor-pointer'
               />
+              <div className='flex justify-between py-2 px-4'>
+                <p className='bg-orange-700 h-18 w-18 rounded-full -mt-10 text-white py-4 px-2 border-8'>{item.price}</p>
+              </div>
             </div>
           ))
         }
